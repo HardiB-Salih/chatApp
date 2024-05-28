40e081ec4aff9305c413cda65c40fb88292de337
HardiB.Salih <hardib.salih@192.168.1.47>
2024-05-28 10:57:10 +0300

    first commit

---
+++ Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version:5.10
+import PackageDescription
+
+let package = Package(
+    name: "chat-app",
+    platforms: [
+       .macOS(.v13)
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.99.3"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+        
+    ],
+    targets: [
+        .executableTarget(
+            name: "App",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+        .testTarget(
+            name: "AppTests",
+            dependencies: [
+                .target(name: "App"),
+                .product(name: "XCTVapor", package: "vapor"),
+            ],
+            swiftSettings: swiftSettings
+        )
+    ]
+)
+
+var swiftSettings: [SwiftSetting] { [
+    .enableUpcomingFeature("DisableOutwardActorInference"),
+    .enableExperimentalFeature("StrictConcurrency"),
+] }
